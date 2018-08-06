6ba90ced806c605899b341c52ab1567f7c8c2960
unknown <gomezger.a9@gmail.com>
2018-08-06 18:38:13 -0300

    Agregado Jumbotron y cambiado el color del fondo

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
 import React, { Component } from 'react';
 import logo from './SVG/logo.svg';
 import linkedin from './SVG/linkedin.svg';
-import github from './SVG/github.svg';
 import mail from './SVG/mail.svg';
 import Particles from 'react-particles-js';
-
 import Fade from 'react-reveal/Fade';
+import Contacto from './components/Contacto/Contacto';
+import Jumbotron from './components/Jumbotron/Jumbotron'
 import './App.css';
 
 const particlesOptionsDiv = {
-  "particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},
+  "particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#000000"},"shape":{"type":"circle","stroke":{"width":0,"color":"#fff345"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},
 }
 
+class App extends React.Component {
+  constructor () {
+    super();
+    this.setState= {
+      route: 'home'
+    }
+  }
 
-
-class App extends Component {
+  onRouteChange = (route) => {
+    this.setState({route: route});
+  }
+  
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          
+          {/* Linkedin */}
+
           <a href="https://www.linkedin.com/in/german-ariel-gomez-781040152/" 
           target ="_blank" 
           rel = "noopener noreferrer">
             <img className= "logo-linkedin" alt="linkedin" src= {linkedin}  />
           </a>
-            {/* Averiguar como hacer para hacer una pagina mas 
-            con onroutechange y hacer componente de contacto */}
-          <img className= "logo-mail" alt= "mail" src={mail}/>
-          <a href = "https://github.com/gg-ballin" className = "logo"
-          target ="_blank" 
-          rel = "noopener noreferrer"> 
-            <img classname= "logo-github" alt ="github" src= {github} />
-          </a>
+            
+          {/* Averiguar como hacer para hacer una pagina mas 
+          con onroutechange y hacer componente de contacto */}
+          {/* Github */}
+
+        
+          <img className= "logo-mail" alt= "mail" src={mail} onRouteChange = {this.onRouteChange}/> 
+         
           <h1 className = "name">
-          <Fade bottom cascade>  
-            Germán Gómez
-          </Fade>
+            <Fade bottom cascade>  
+              Germán Gómez
+            </Fade>
           </h1>
           <p className = "desc">
           <Fade bottom cascade> 
@@ -43,11 +56,14 @@ class App extends Component {
           </Fade>
           </p>
         </header>
-        
-          <div>
-          <Particles className = 'particles'
-            params = {particlesOptionsDiv}
-          />
+        <Particles className = 'particles'
+              params = {particlesOptionsDiv}
+            />
+          <div className = "App">
+            <div className = "App">
+            
+            </div>
+            <Jumbotron />
           <Fade right big cascade>  
             <p className = "mg">Me gusta hacer páginas web interactivas.</p>
           </Fade>
@@ -60,7 +76,7 @@ class App extends Component {
              
           <p className = "OS"> 
           <Fade right big cascade>
-          Open Source enthusiast.
+          Open Source enthusiast. | 2018.
           </Fade>
           </p>
         </footer>
---
+++ src/components/Contacto/Contacto.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Contacto extends React.Component {
+    render() {
+        return(
+            <div> 
+                <legend>gomezger.a9@gmail.com </legend>
+            </div>
+
+
+        );
+    }
+}
+
+export default Contacto;
